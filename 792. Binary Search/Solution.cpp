5cfa5644e0b6ef6cda9cdf9a35fa1c1c4abe4ca6
Priyanshu Uchat <85685604+ITSMEPRIYANSHUUCHAT@users.noreply.github.com>
2024-08-06 22:16:11 +0530

    C++ Solution
            Submission Statistics:
            Question #: 792
            Language: C++
            Runtime: 29 ms
            Runtime percentile: 37.43
            Memory: 30.188 MB
            Memory percentile: 31.97

---
+++ 792. Binary Search/Solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int high=nums.size()-1;
+        int low=0;
+        int mid=nums.size()/2;
+        cout<<mid<<endl;
+        if(nums.size()==1)
+        {
+            if(nums[0]==target)
+            {
+                return 0;
+            }
+            else return -1;
+        }
+        while(low<=high)
+        {
+            if( nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(target>nums[mid])
+            {
+                low=mid+1;
+                mid=(low+high)/2;
+            }
+            else if(target<nums[mid])
+            {
+                high=mid-1;
+                mid=(low+high)/2;
+            }
+            cout<< mid<<endl;
+        
+        }
+        return -1;
+    }
+};
