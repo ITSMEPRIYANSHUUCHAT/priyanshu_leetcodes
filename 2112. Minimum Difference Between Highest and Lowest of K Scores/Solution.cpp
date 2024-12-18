1dec10a9d8bfb2d23cd65392b51cb4d18d5e10b2
Priyanshu Uchat <85685604+ITSMEPRIYANSHUUCHAT@users.noreply.github.com>
2024-12-18 13:27:04 +0530

    C++ Solution
            Submission Statistics:
            Question #: 2112
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 17.204 MB
            Memory percentile: 18.99

---
+++ 2112. Minimum Difference Between Highest and Lowest of K Scores/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=k-1;
+        int mini=INT_MAX;
+
+        while(j<nums.size())
+        {
+            mini=min(mini,nums[j]-nums[i]);
+            i++;
+            j++;
+        }
+
+        return mini;
+    }
+};
